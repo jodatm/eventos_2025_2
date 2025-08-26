a65d695eecd4a70e0251856a2969d66becac9cdd
jodatm <joshuadtm@gmail.com>
2025-08-26 16:13:37 -0500

    clase 1

---
+++ clase1/src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) {
+        int edades [] = {20,30,25};
+        for(int edad : edades){
+            int nuevaEdad = (int)(Math.random()*10+edad);
+            System.out.println(nuevaEdad);
+        }
+        int matriz [][] = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+        for(int i = 0; i < matriz.length ; i++){
+            for(int j = 0; j < matriz[i].length ; j++){
+                System.out.println(matriz[i][j]);
+            }
+        }
+    }
+}
